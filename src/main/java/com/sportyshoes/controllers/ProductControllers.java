5d37377cf5e9e11ef0d339c36a81016c2e55b8a8
pjosephraj <mailjosephraj@gmail.com>
2022-10-14 22:08:07 +0530

    ...

--- src/main/java/com/sportyshoes/controllers/ProductControllers.java
+++ src/main/java/com/sportyshoes/controllers/ProductControllers.java
@@ -3,7 +3,11 @@ package com.sportyshoes.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,29 +23,34 @@ public class ProductControllers {
    @Autowired
    ProductRepository repository;
 
-   @RequestMapping(value = "", method = RequestMethod.GET)
+   @GetMapping(value = "")
    public List<Product> getProducts() {
       return repository.findAll();
    }
 
-   @RequestMapping(value = "{id}", method = RequestMethod.GET)
+   @GetMapping(value = "{id}")
    public Product getProduct(@PathVariable("id") int id) {
       return repository.findById(id).get();
    }
 
-   @RequestMapping(value = "", method = RequestMethod.POST)
+   @PostMapping(value = "")
    public Product createProduct(@RequestBody Product product) {
       return repository.save(product);
    }
 
-   @RequestMapping(value = "", method = RequestMethod.PUT)
+   @PutMapping(value = "")
    public Product updateProduct(Product product) {
       return repository.save(product);
    }
 
-   @RequestMapping("{id}")
+   @DeleteMapping("{id}")
    public void deleteProduct(@PathVariable("id") int id) {
       repository.deleteById(id);
    }
+
+//   @GetMapping(value="create-table")
+//   public void createTable() {
+//
+//   }
 
 }
--- src/main/java/com/sportyshoes/views/ProductViews.java
+++ src/main/java/com/sportyshoes/views/ProductViews.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,7 +29,7 @@ public class ProductViews {
    @Autowired
    ProductControllers productControllers;
 
-   @RequestMapping(value = "/products", method = RequestMethod.GET)
+   @GetMapping(value = "/products")
    public ModelAndView productList() {
       ModelAndView mnv = new ModelAndView("product-list");
       List<Product> products = productControllers.getProducts();
@@ -42,25 +43,31 @@ public class ProductViews {
       return "product-add";
    }
 
-   @RequestMapping(value = "/add-product", method = RequestMethod.POST)
-   public ModelAndView saveProduct(@ModelAttribute(name="productForm") Product product, Model model ) {
-
-      productControllers.createProduct(product);
-      ModelAndView mnv = new ModelAndView("product-list");
-      List<Product> products = productControllers.getProducts();
-      //      List<Product> noProducts = new ArrayList<>();
-      mnv.addObject("products", products);
-      mnv.addObject("success", true);
-      mnv.addObject("message", "Products Successfully added.");
-      mnv.addObject("isAdmin", true);
+   @GetMapping("/products/{id}")
+   public ModelAndView editProduct(@PathVariable("id") int id) {
+      ModelAndView mnv = new ModelAndView("product-add");
+      Product product = productControllers.getProduct(id);
+      mnv.addObject("productId", product.getId());
+      mnv.addObject("productName", product.getProduct_name());
+      mnv.addObject("productBrand", product.getBrand());
+      mnv.addObject("productColor", product.getColor());
+      mnv.addObject("productPrice", product.getPrice());
+      mnv.addObject("productSeason", product.getSeason());
+      mnv.addObject("productCategory", product.getCategory());
+      mnv.addObject("productDiscount", product.getDiscount());
       return mnv;
    }
 
-   @RequestMapping(value="/delete-product", method=RequestMethod.POST)
+   @PostMapping(value = "/add-product")
+   public RedirectView saveProduct(@ModelAttribute(name="productForm") Product product, RedirectAttributes ra ) {
+      productControllers.createProduct(product);
+      return listProducts(true, "Product Added Successfully!", ra);
+   }
+
+   @PostMapping(value="/delete-product")
    public RedirectView deleteProduct(@ModelAttribute(name="productForm") Product product, RedirectAttributes ra) {
-      System.out.println(product);
       productControllers.deleteProduct(product.getId());
-      return listProducts(true, "Product Deleted Successfully!", ra);
+      return listProducts(false, "Product Deleted Successfully!", ra);
    }
 
    public RedirectView listProducts(boolean isSuccess, String message, RedirectAttributes ra) {
